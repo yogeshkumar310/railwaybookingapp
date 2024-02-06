0ee1605d489f83caca942acddccf84fa6016f638
yogesh <iyogeshk31@gmail.com>
2024-02-06 12:17:43 +0530

    hello

---
+++ src/main/java/com/jsp/railwaybookingapp/RailwaybookingappApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.railwaybookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwaybookingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwaybookingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsp/railwaybookingapp/RailwaybookingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.railwaybookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwaybookingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
